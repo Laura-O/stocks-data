aad894e539de96d512ad328613365d46f8993e3d
Laura Ohrndorf <laura.ohrndorf@gmail.com>
2022-02-02 17:01:26 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+from argparse import ArgumentParser
+from source.fmp import ProfileFMP
+
+
+data_dir = os.path.join(os.path.dirname(__file__), 'data')
+if not os.path.isdir(data_dir):
+    os.makedirs(data_dir)
+
+def fetch_data_by_symbol(symbol):
+    fmp_company = ProfileFMP(symbol)
+    
+    return {
+            'symbol': symbol,
+            'profile': fmp_company.profile,
+            'rating': fmp_company.rating,
+            }
+
+def load(symbol):
+    company = fetch_data_by_symbol(symbol)
+    filename = os.path.join(data_dir, symbol + '.json')
+    
+    with open(filename, 'w') as file:
+        json.dump(company, file)
+    
+    return company
+
+parser = ArgumentParser()
+subparsers = parser.add_subparsers(dest="action", title='Subcommands')
+
+load_parser = subparsers.add_parser('load', help='laod data')
+load_parser.add_argument('symbols', type=str, nargs='*', help='stock symbol')
+
+args = sys.argv[1:]
+args = parser.parse_args(args)
+
+if args.action == 'load':
+    symbols = args.symbols
+
+    for symbol in symbols:
+        print("Loading data for {}.".format(symbol))
+        load(symbol)
+    sys.exit(0)
+else:
+    parser.error('Unknown command: ' + repr(args.action))
---
+++ source/fmp.py
@@ -0,0 +1,28 @@
+import json
+import certifi
+import os
+from urllib.request import urlopen
+
+API_KEY = os.environ.get("FMP_KEY")
+
+profile_url = 'https://financialmodelingprep.com/api/v3/profile/%s?apikey=' +  API_KEY
+rating_url = 'https://financialmodelingprep.com/api/v3/rating/%s?apikey=' + API_KEY
+
+def get_jsonparsed_data(url):
+    response = urlopen(url, cafile=certifi.where())
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+class ProfileFMP(object):
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.set_profile(symbol)
+        self.set_rating(symbol)
+
+    def set_profile(self, symbol):        
+        data = get_jsonparsed_data(profile_url % symbol)
+        self.profile = data[0]
+
+    def set_rating(self, symbol):        
+        data = get_jsonparsed_data(rating_url % symbol)
+        self.rating = data[0]
